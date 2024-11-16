3b90259dd837dfea8591ab6c4c73cf40b457f2c6
Timur Usmanov <t.usmanov@innopolis.university>
2024-11-16 19:22:52 +0300

    Update commands for configs

--- lua/timur/commands.lua
+++ lua/timur/commands.lua
@@ -2,6 +2,13 @@ vim.api.nvim_create_user_command(
     "Vscode",
     "!cp -r ~/.config/nvim/cpp.vscode ./.vscode",
     {
-        desc = "Copy the default configuration to the current directory."
+        desc = "Copy the default C++ configuration to the current directory."
+    }
+)
+vim.api.nvim_create_user_command(
+    "VscodeC",
+    "!cp -r ~/.config/nvim/c.vscode ./.vscode",
+    {
+        desc = "Copy the default C configuration to the current directory."
     }
 )
