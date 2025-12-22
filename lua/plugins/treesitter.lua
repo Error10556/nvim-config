9ed2f07ec4d160a1038bcfa67f49feaabd9f1d49
Timur Usmanov <t.usmanov@innopolis.university>
2025-12-22 19:57:19 +0300

    Update treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,10 @@
 return {{
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
-
-      configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "html", "xml", "cpp", "desktop", "bash", "python", "sql", "regex", "json", "gitignore", "asm" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end
+    lazy = false,
+    config = function()
+        require'nvim-treesitter'.install{ "c", "lua", "vim", "vimdoc", "html",
+        "xml", "cpp", "desktop", "bash", "python", "sql", "regex", "json",
+        "gitignore", "asm" }
+    end,
 }}
