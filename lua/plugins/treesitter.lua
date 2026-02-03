5c032e460dc23c1aebbf8cdc1bea1581114ad839
Timur Usmanov <t.usmanov@innopolis.university>
2026-02-03 11:04:02 +0300

    Enable treesitter highlighting, indents, and folding for C++, python, haskell, and go

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,5 +6,18 @@ return {{
         require'nvim-treesitter'.install{ "c", "lua", "vim", "vimdoc", "html",
         "xml", "cpp", "desktop", "bash", "python", "sql", "regex", "json",
         "gitignore", "asm" }
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = {
+                'python', 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'python',
+                'go', 'gomod', 'gowork', 'gotmpl', 'haskell', 'lua', 'typst'
+            },
+            callback = function()
+                vim.treesitter.start()
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+                vim.wo[0][0].foldmethod = 'expr'
+                vim.wo[0][0].foldlevel = 99
+            end
+        })
     end,
 }}
