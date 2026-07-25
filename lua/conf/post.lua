a04b5d7979bdd3faeb201b87a755dd7638c29f84
Timur Usmanov <t.usmanov@innopolis.university>
2026-07-25 21:44:37 +0300

    Highlight spaces at ends of lines

--- lua/conf/post.lua
+++ lua/conf/post.lua
@@ -1,5 +1,17 @@
 require('conf.configlsp')
 
+vim.cmd([[highlight TrailingWhitespace ctermbg=red guibg=red]]);
+vim.api.nvim_create_autocmd({'ColorScheme'}, {
+    pattern = '*',
+    command = [[highlight TrailingWhitespace ctermbg=red guibg=red]]
+});
+vim.api.nvim_create_autocmd({'BufNew'}, {
+    pattern = '*',
+    callback = function(ev)
+        vim.fn.matchadd("TrailingWhitespace", [[\s\+$]])
+    end
+});
+vim.fn.matchadd("TrailingWhitespace", [[\s\+$]]);
 local ok, _ = pcall(require, 'conf.custom')
 if not ok then
     vim.cmd[[colorscheme melange]]
