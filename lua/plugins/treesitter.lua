0230af16eb76a8ff5f8e7d92ab8c0dfdbe388d39
Timur Usmanov <t.usmanov@innopolis.university>
2026-08-31 16:53:09 +0300

    Revert "Migrate to tree-sitter-manager; fix broken haskell parser"
    This reverts commit e80f4a2d6d580c4a750896e54a1f9f7a90f0a2b2.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,21 +1,23 @@
 return {{
-    "romus204/tree-sitter-manager.nvim",
-    dependencies = {}, -- tree-sitter CLI must be installed system-wide
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    lazy = false,
     config = function()
-        require("tree-sitter-manager").setup({
-            ensure_installed = {
-                "c", "lua", "vim", "vimdoc", "html", "xml", "cpp", "desktop",
-                "bash", "python", "sql", "regex", "json", "gitignore", "asm",
-                "typst", "haskell"
+        require'nvim-treesitter'.install{ "c", "lua", "vim", "vimdoc", "html",
+        "xml", "cpp", "desktop", "bash", "python", "sql", "regex", "json",
+        "gitignore", "asm", "typst", "haskell" }
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = {
+                'python', 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'python',
+                'go', 'gomod', 'gowork', 'gotmpl', 'haskell', 'lua', 'typst'
             },
-            auto_install = true,
-            languages = { haskell = {
-                install_info = {
-                    revision = "98aedbd2d6947a168ba3ba3755d70b0cb6b78395",
-                    url = "https://github.com/tree-sitter-grammars/" ..
-                        "tree-sitter-haskell",
-                }},
-            },
+            callback = function()
+                vim.treesitter.start()
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+                vim.wo[0][0].foldmethod = 'expr'
+                vim.wo[0][0].foldlevel = 99
+            end
         })
     end,
 }}
